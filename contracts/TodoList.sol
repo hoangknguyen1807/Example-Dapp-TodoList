5085f08b6b775705dffe3b81142a72ae43da9b94
Lucas Hoang Nguyen <hoangknguyen.1807@gmail.com>
2022-02-18 14:50:12 +0700

    Finished feature add and delete task items

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint256 public taskCount = 0;
 
     struct Task {
-        uint id;
+        uint256 id;
         string content;
-        bool completed;        
+        bool completed;
     }
 
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Hello! This is a new task in your To-Do list");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    function deleteTask(uint256 _id) public {
+        for (uint256 i = _id; i < taskCount; i++) {
+            tasks[i] = tasks[i + 1];
+        }
+        taskCount--;
+    }
 }
